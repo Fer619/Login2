05bd234994ed909b502b7a190ba41d1d37f21c61
Fernando Briceño <73617750+Fer619@users.noreply.github.com>
2023-03-08 19:28:32 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,23 @@
 <?php
 session_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $captcha = $_POST['captcha'];
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	// Verificar si el captcha ya ha sido completado
-	if (isset($_SESSION["captcha_completed"]) && $_
+  // Validar el captcha
+  if ($_SESSION['captcha'] != $captcha) {
+    $error = "El código captcha es incorrecto";
+  }
+
+  // Validar el correo electrónico y la contraseña
+  if ($email == 'correo@example.com' && $password == 'contraseña123') {
+    // Iniciar sesión
+    $_SESSION['email'] = $email;
+    header('Location: perfil.php');
+    exit();
+  } else {
+    $error = "El correo electrónico o la contraseña son incorrectos";
+  }
+}
+?>
